0f2f4d73e056014eb7592fccc5b7565d394daf74
abeliojuniar <abelio.juniar@students.itb.ac.id>
2016-11-01 16:56:18 +0700

    FIP_m add initial route plot

--- FIP.py
+++ FIP.py
@@ -4,26 +4,40 @@
 # Author : Abelio
 ###########################
 import tkinter
-from defproc import *
-from tkinter import * 
-import matplotlib
-matplotlib.use('TkAgg')
-import numpy as np
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
-import sys
+from tkinter import *
+import math as mat
 
+# Procedure to get required parameters from "parameter.txt"
+def getVarFromFile(filename):
+	import imp
+	f = open(filename)
+	global data
+	data = imp.load_source('data', '', f)
+	f.close()
+
+# Procedure to print all parameters in "parameter.txt"
+def printParam():
+	global data
+	print("beta = ",data.beta)
+	print("gamma2 = ",data.gamma2)
+	print("gamma3 = ",data.gamma3)
+	print("gamma4 = ",data.gamma4)
+
+# Calculate distance
+def calculateDistance(x1,y1,x2,y2):
+	return mat.sqrt(pow((x2-x1),2) + pow((y2-y1),2))
+	
 class Window (Frame):
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
 		self.master=master
 		self.init_window()
-		self.createWidgets()
 	
 	def init_window(self):
 		self.master.title("FREIGHT INSERTION PROBLEM")
 		self.pack(fill=BOTH, expand=1)
+		#quitButton = Button(self,text="Quit",command=root.destroy)
+		#quitButton.place(x=500, y=500)
 		
 		menu = Menu(self.master)
 		self.master.config(menu=menu)
@@ -35,34 +49,15 @@ class Window (Frame):
 		edit = Menu(menu)
 		edit.add_command(label='Edit config...')
 		menu.add_cascade(label='Edit', menu=edit)
-		
-		self.createWidgets()
-		self.quitButton = Button(master=root,text="Quit",command=root.destroy)
-		self.quitButton.place(x=550, y=550)
 	
-	def createWidgets(self):
-		# fig=plt.figure(figsize=(8,8))
-		# ax=fig.add_axes([0.1,0.1,0.8,0.9],polar=False)
-		# canvas=FigureCanvasTkAgg(fig,master=root)
-		# canvas.get_tk_widget().grid(row=0,column=1)
-		# canvas.show()
-		fig, ax = plt.subplots()
-		#plt.subplots_adjust(left=0.25, bottom=0.25)
-		plt.axis([-25, 25, -25, 25])
-		#self.plotbutton=Button(master=root, text="plot", command=lambda: self.showPlot())
-		#self.plotbutton.grid(row=0,column=0)
-		#plt.plot([4,3,2,3,4])
-		#plt.ylabel('some numbers')
-		plt.show()
-	
-	#def showPlot(self):
 
-
-	
+getVarFromFile('./parameter.txt')
+getVarFromFile('./position.txt')
 
 ##printParam()
 ##input("\nPress <ENTER> to end program")
+
 root = Tk()
-#root.geometry("400x400")
+root.geometry("600x600")
 app = Window(root)
-#root.mainloop()
+root.mainloop()
--- FIP_m.py
+++ FIP_m.py
@@ -3,43 +3,94 @@
 # Description : Main program of FIP
 # Author : Abelio
 ###########################
-import tkinter
-from defproc import *
-#from tkinter import * 
-import matplotlib
-matplotlib.use('TkAgg')
-import numpy as np
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.widgets import Slider, Button, RadioButtons
+import math as mat
 import matplotlib.pyplot as plt
-import sys
+import numpy as np
+from matplotlib.widgets import Slider, Button, RadioButtons
 
-#n = input('Jumlah barang = ')
-#m = input('Jumlah penumpang = ')
-#absK = input('Jumlah mobil yang beroperasi = ')
+# K = set of cars
+class setCar(object):
+	def __init__(self):
+		self.start=None
+		self.V=[]
+		self.Vpo=[]
+		self.Vpd=[]
+		self.Vfo=[]
 
+# Calculate distance
+def calculateDistance(x1,y1,x2,y2):
+	return mat.sqrt(pow((x2-x1),2) + pow((y2-y1),2))
+	
+def readCSV(filename):
+	import csv
+	global column
+	column=[]
+	with open(filename) as f:
+		reader = csv.reader(f)
+		for row in reader:
+			column.extend(row)
+		for i in range(0,len(column)):
+			column[i]=column[i].split(';')
+
+def setVariable(column):
+	# Vpo = passenger origins
+	# Vpd = passenger destinations 
+	# Vfo = freight origins
+	# Vfd = freight destinations
+	global K,Vpo,Vpd,Vfo,Vfd
+	K=[setCar() for i in range(int(column[2][4]))]
+	foundCar=-1
+	Vpo=[];Vpd=[];Vfo=[];Vfd=[]
+	for i in range(8,len(column)):
+		if(column[i][0]=='Car'):
+			foundCar=foundCar+1
+			K[foundCar].start = [float(column[i][1]),float(column[i][2])]
+		elif(column[i][0]=='Passenger'):
+			K[foundCar].Vpo.append([float(column[i][1]),float(column[i][2])])
+			K[foundCar].Vpd.append([float(column[i][3]),float(column[i][4])])
+		elif(column[i][0]=='Freight'):
+			K[foundCar].Vfo.append([float(column[i][1]),float(column[i][2])])
+			K[foundCar].Vfd.append([float(column[i][3]),float(column[i][4])])
+			
+def plotInitialRoute(K):
+	print(K[0].start[1])
+	routeX=[0 for i in range(len(K))]
+	routeY=[0 for i in range(len(K))]
+	orX=[]
+	orY=[]
+	desX=[]
+	desY=[]
+	startX=[]
+	startY=[]
+	for i in range(len(K)):
+		routeX[i]=[K[i].start[0]]
+		routeY[i]=[K[i].start[1]]
+		startX.append(K[i].start[0])
+		startY.append(K[i].start[1])
+		for j in range(len(K[i].Vpo)):
+			routeX[i].append(K[i].Vpo[j][0])
+			routeY[i].append(K[i].Vpo[j][1])
+			routeX[i].append(K[i].Vpd[j][0])
+			routeY[i].append(K[i].Vpd[j][1])
+			orX.append(K[i].Vpo[j][0])
+			orY.append(K[i].Vpo[j][1])
+			desX.append(K[i].Vpd[j][0])
+			desY.append(K[i].Vpd[j][1])
+	#ppo, = plt.plot(xpo,ypo,'ro--')
+	line0, = plt.plot(routeX[0],routeY[0],'g-')
+	line1, = plt.plot(routeX[1],routeY[1],'m-')
+	line0, = plt.plot(orX,orY,'bo')
+	line1, = plt.plot(desX,desY,'ro')
+	start = plt.plot(startX,startY,'ko')
+	
+readCSV('./data.csv')
 fig, ax = plt.subplots()
-t = np.arange(0.0, 1.0, 0.001)
-a0 = 5
-f0 = 3
-s = a0*np.sin(2*np.pi*f0*t)
-l, = plt.plot([15, 22, 0], [-21, 21, 0], lw=2, color='red')
-
 plt.axis([-25,25,-25,25])
-
-axcolor = 'lightgoldenrodyellow'
-resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax,'Plot', color=axcolor, hovercolor='0.975')
-
-
-param=getVarFromFile('parameter.txt')
-getVarFromFile('position.txt')
-print(param.absK)
-print(param.gamma2)
-#printParam()
-
-#print(data.beta)
-
-
+setVariable(column)
+#print(K[0].Vpo)
+#print(K[1].Vpo)
+plotInitialRoute(K)
+#plotPas()
+plt.axis([-25,25,-25,25])
+plt.grid(True)
 plt.show()
